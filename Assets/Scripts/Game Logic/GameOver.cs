20400c2d92db94d4e0cca27953f3ce8f99a95c1f
Rémi Raffaghello <remi_raffaghello@hotmail.com>
2023-09-27 16:08:14 +0200

    GameOver resets the score

--- Assets/Scripts/Game Logic/GameOver.cs
+++ Assets/Scripts/Game Logic/GameOver.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TNRD;
 
 /// <summary>
 /// Restarts the level when the player falls out the ground.
 /// </summary>
 public class GameOver : MonoBehaviour
 {
+    [Header("Data")]
+    [SerializeField] private SerializableInterface<IReset> _scoreResetSerialized;
+
+    // Properties for the interfaces
+    private IReset _scoreReset => _scoreResetSerialized.Value;
+
     // Player is out
     public void OnPlayerExit()
     {
-        // TODO reset score
-        Debug.Log("Restart level");
+        // Reset the score
+        _scoreReset.Reset();
+
+        Debug.Log("Restart level");        
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
