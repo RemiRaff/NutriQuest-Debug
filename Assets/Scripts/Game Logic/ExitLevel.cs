1fc682b1b8ecef746fbf5316c9e078f16bf26a77
Rémi Raffaghello <remi_raffaghello@hotmail.com>
2023-09-27 16:09:26 +0200

    ExitLevel restarts the level (without score reseting)

--- Assets/Scripts/Game Logic/ExitLevel.cs
+++ Assets/Scripts/Game Logic/ExitLevel.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Exits the level when the player reaches the end.
@@ -9,5 +10,7 @@ public class ExitLevel : MonoBehaviour
     {
         // TODO : Exit the level
         Debug.Log("Exit level");
+        // Reload current level, TODO : create new level and load it (Fade sceen)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
